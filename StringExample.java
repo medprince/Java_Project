90949cbb7a35c8dfc755873e9e418c770271ef70
medprince <mohamedsamuelkamara60gmail.com>
2023-11-14 11:29:24 -0500

    String Example

---
+++ StringExample.java
@@ -0,0 +1,31 @@
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		
+//		String brand = "pwskills";
+//		brand.concat("Ohio");
+//		System.out.println(brand);
+//		StringBuffer brand1 = new StringBuffer("Mohamed");
+//		brand1.append(" Samuel Kamara");
+//		System.out.println(brand1);
+		
+//		String s1 = "Pwskills";
+//		String s2 = new String("Pwskills");
+//		System.out.println(s1 == s2);
+//		System.out.println(s1.equals(s2));
+//		String newString = new String("Mohamed");
+//		newString = newString.concat("Samuel Kamara");
+//		System.out.println(newString + " testing");
+		
+		String s1="pw";
+		String s2 = "pw" + "java";
+		String s3 = "pw" + "java" + "Freetown";
+		String s4 = s1 + s2;
+		System.out.println("s1: " +s1);
+		System.out.println("s2: " +s2);
+		System.out.println("s3: "+ s3);
+		System.out.println("s4: " + s4);
+	}
+
+}
