7681e4bf23a84ef1e23404c730567912d0517cff
medprince <mohamedsamuelkamara60gmail.com>
2023-11-14 09:32:29 -0500

    Array Example

---
+++ ArrayDemo.java
@@ -0,0 +1,20 @@
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int ages[] = {32,56,57,7,29,35}; 
+		
+//		for(int i =0; i<nums.length; i++) {
+//			System.out.println(nums[i]);
+//		}
+		String names[] = {"Mohamed", "Samuel", "Kamara", "Samsatu", "Medcilia", "Prince Med"};
+		
+		for(int i=0; i<names.length; i++ ) {
+			System.out.println(names[i] + ": " + ages[i]);
+		}
+
+		
+
+	}
+
+}
