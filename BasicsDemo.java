2cc34c0a3cbf6a639628c481f8a9229233a5c0dc
medprince <mohamedsamuelkamara60gmail.com>
2023-11-18 04:39:42 -0500

    Java BAsic Indepth

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -43,30 +43,55 @@
       
       // Information loss due to out-of-range assignment
       byte narrowdByte = (byte)123456;
-	  	System.out.println("narrowdByte: " + narrowdByte); 
+	  System.out.println("narrowdByte: " + narrowdByte); 
 	  
 	  // Truncation
-			int iTruncated = (int)0.99;
-			System.out.println("iTruncated: " + iTruncated); 
-			
-			// Implicit cast (int to long)
-			y = x;
-			
-			// Implicit cast (char to int)
-			char cChar = 'A';
-			int iInt = cChar; 
-			System.out.println("iInt: " + iInt);
+	  int iTruncated = (int)0.99;
+	  System.out.println("iTruncated: " + iTruncated); 
+	  
+	  // Implicit cast (int to long)
+	  y = x;
+	  
+	  // Implicit cast (char to int)
+	  char cChar = 'A';
+	  int iInt = cChar; 
+	  System.out.println("iInt: " + iInt);
 	  
 	  // byte to char using an explicit cast
-			byte bByte = 65;
-			cChar = (char)bByte; // special conversion (widening from byte --> int followed by narrowing from int --> char)
-			System.out.println("cChar: " + cChar);
-			}
+	  byte bByte = 65;
+	  cChar = (char)bByte; // special conversion (widening from byte --> int followed by narrowing from int --> char)
+	  System.out.println("cChar: " + cChar);
+    }
+	static void arrays(){
+		System.out.println("\nInside array ...");
+		/* int[] scores = new int[4];
+		scores[0] = 90;
+		scores[1] = 70;
+		scores[2] = 80;
+		scores[3] = 100;*/
+		//int[] scores = new int[] {90, 70, 80, 100};
+		int[] scores = {90, 70, 80, 100};
+		System.out.println("Midterm 1: " + scores[0]);
+		System.out.println("Midterm 2: " + scores[1]);
+		System.out.println("Final: " + scores[2]);
+		System.out.println("Project: " + scores[3]);
 		
-		public static void main(String[] args) {	
-			// Language Basics 1
-			//print();				
-			//primitives();
-			typeCasting(); 
-			}       
+		Student[] students =  new Student[] {new Student(), new Student(), new Student()};
+		/*students[0] = new Student();
+		students[1] = new Student();
+		students[2] = new Student();*/
+		students[0].name = "John";
+		students[1].name = "Abu";
+		students[2].name = "Prince Med";
+		System.out.println("Student 1: " + students[0].name);
+		System.out.println("Student 2: " + students[1].name);
+		System.out.println("Student 3: " + students[2].name);
+	}
+	public static void main(String[] args) {	
+	  // Language Basics 1
+		arrays();
+	  //print();				
+	  //primitives();
+	  //typeCasting(); 
+    }       
   }
